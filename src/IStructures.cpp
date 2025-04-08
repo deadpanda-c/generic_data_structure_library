a4c9e9105cf5456660e6e66035562922aab9a2dc
Clément Lagier <clement.lagier@epitech.eu>
2025-04-08 14:44:45 +0900

    [ADD] Added a temporary IStructures to make the project compiling

---
+++ src/IStructures.cpp
@@ -0,0 +1,11 @@
+#include "IStructures.hpp"
+
+IStructures::IStructures() {
+    // Constructor implementation
+  std::cout << "IStructures object created." << std::endl;
+}
+
+IStructures::~IStructures() {
+    // Destructor implementation
+  std::cout << "IStructures object destroyed." << std::endl;
+}
